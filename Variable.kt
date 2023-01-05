0d644b2d3b8bcd23d21f333ee94697bdb8714cf3
harsh6975 <harshu1234ashv@gmail.com>
2023-01-05 21:35:44 +0530

    val and var

---
+++ Variable.kt
@@ -0,0 +1,20 @@
+fun main() {
+
+    // variable with data type
+    val name: String = "Harsh Sinha"
+    println(name)
+
+    // variable without data type
+    val name1 = "Harsh Sinha"
+    println(name1)
+
+    // val is immutable i.e cann't be reassinged
+    // name1="Harsh"
+
+    // var is mutable i.e can be reassinged
+    var name2 = "Harsh Sinha"
+    println(name2)
+
+    name2 = "Sinha"
+    println(name2)
+}
